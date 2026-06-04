fce921aec170635d56cb3e6679789757f9d139ff
svc <syeongni@naver.com>
2026-06-04 22:25:56 +0900

    from video to gif

--- player/script.js
+++ player/script.js
@@ -1,31 +1,31 @@
-let player;
+// let player;
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        videoId: 'ink3rZtrTGQ',
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         videoId: 'ink3rZtrTGQ',
 
-        playerVars: {
-            autoplay: 0,
-            // mute: 1,
-            modestbranding: 1,
-            rel: 0
-        }
-    });
-}
+//         playerVars: {
+//             autoplay: 0,
+//             // mute: 1,
+//             modestbranding: 1,
+//             rel: 0
+//         }
+//     });
+// }
 
-function playVideo() {
-    player.playVideo();
-}
+// function playVideo() {
+//     player.playVideo();
+// }
 
-function pauseVideo() {
-    player.pauseVideo();
-}
+// function pauseVideo() {
+//     player.pauseVideo();
+// }
 
-function toggleMute() {
-    if(player.isMuted()) {
-        player.unMute();
-    }
-    else {
-        player.mute();
-    }
-}
+// function toggleMute() {
+//     if(player.isMuted()) {
+//         player.unMute();
+//     }
+//     else {
+//         player.mute();
+//     }
+// }
